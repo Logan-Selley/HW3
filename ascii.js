cc6602f859cbdd8df25dcc8d84f3bdc2484e0579
Logan Selley <archane24@gmail.com>
2018-04-18 14:43:22 -0700

    init

---
+++ ascii.js
@@ -0,0 +1,24 @@
+/*
+
+*/
+window.onload = function() {
+    $(start).onclick = start;
+    $(stop).onclick = stop;
+    alert("test");
+}
+
+start() {
+    alert("test1");
+}
+
+animate() {
+
+}
+
+stop() {
+
+}
+
+$(id) {
+    return document.getElementById(id)
+}
